9802392c18042972cb31079d042c410b3c043a2b
Nicitaa <icpcsenondaryemail@gmail.com>
2023-04-18 21:06:27 +0200

    1)Routing

--- shopping-cart_practice_1/src/App.tsx
+++ shopping-cart_practice_1/src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import {Home} from "./pages/Home";
+import {Store} from "./pages/Store";
+import {About} from "./pages/About";
+import {Container} from "react-bootstrap";
 
 function App() {
 
   return (
-   <h1>Hi</h1>
+    <Container className='mb-4'>
+   <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/store' element={<Store/>}/>
+     <Route path='/about' element={<About/>}/>
+   </Routes>
+    </Container>
   )
 }
 
