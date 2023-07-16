a7b78404a73d806509e4591d46c310a7df32c058
Nicitaa <icpcsenondaryemail@gmail.com>
2023-07-16 15:36:31 +0200

    Store page 2/6

---
+++ shopping-cart_practice_1/src/components/StoreItem.tsx
@@ -0,0 +1,46 @@
+import { formatCurrency } from '../utils/currencyFormater'
+
+interface StoreItemProps {
+  id: number
+  name: string
+  price: number
+  imgUrl: string
+}
+
+
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+
+  const quantity = 0
+
+  return (
+    <div>
+      <div className='shadow-md border-[1px] border-solid border-gray-400 rounded-md overflow-hidden' key={id}>
+
+        <img className='h-[200px] w-full object-cover' src={imgUrl} alt={name} />
+
+        <div className='flex justify-between text-2xl mb-2 px-2'>
+          <h1>{name}</h1>
+          <h1 className='text-gray-500'>{formatCurrency(price)}</h1>
+        </div>
+        <div className='mt-auto'>
+          {quantity === 0
+            ? <div className='bg-green-600 px-4 py-2 text-xl text-center'>+ Add to Cart</div>
+            : <div>
+              <div className='flex flex-col items-center gap-y-2 px-4 py-2'>
+                <div className='flex flex-row gap-x-4 border-[1px] border-solid border-gray-400 rounded-md overflow-hidden'>
+                  <button className='bg-green-500 w-8 h-8'>+</button>
+                  <h1 className='text-2xl text-gray-400'>{quantity}</h1>
+                  <button className='bg-red-500 w-8 h-8'>-</button>
+                </div>
+                <div className='flex flex-row justify-center'>
+                  <button className='bg-red-500 w-full px-4 py-2 rounded '>Remove</button>
+                </div>
+              </div>
+            </div>
+          }
+        </div>
+      </div>
+    </div>
+
+  )
+}
--- shopping-cart_practice_1/src/pages/StorePage.tsx
+++ shopping-cart_practice_1/src/pages/StorePage.tsx
@@ -1,5 +1,16 @@
+import { StoreItem } from '../components/StoreItem'
+import storeItems from '../data/items.json'
+
+
 export function StorePage() {
   return (
-    <div>Store page</div>
+    <div className='grid grid-cols-4 gap-4 py-16 px-8'>
+      {storeItems.map(item => (
+        <div key={item.id}>
+          <StoreItem {...item} />
+        </div>
+      ))
+      }
+    </div >
   )
 }
