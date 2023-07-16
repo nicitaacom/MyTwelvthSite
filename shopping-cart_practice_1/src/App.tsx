6fd756f7c04c67fd4ddd3a3eb8008db78220a9c5
Nicitaa <icpcsenondaryemail@gmail.com>
2023-07-16 14:47:58 +0200

    Navbar

--- shopping-cart_practice_1/src/App.tsx
+++ shopping-cart_practice_1/src/App.tsx
@@ -2,15 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import { RootPage } from './pages/RootPage'
 import { StorePage } from './pages/StorePage'
 import { AboutPage } from './pages/AboutPage'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<RootPage />} />
-      <Route path='/store' element={<StorePage />} />
-      <Route path='/about' element={<AboutPage />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<RootPage />} />
+        <Route path='/store' element={<StorePage />} />
+        <Route path='/about' element={<AboutPage />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ shopping-cart_practice_1/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+
+import { BiCart } from 'react-icons/bi'
+
+export function Navbar() {
+  return (
+    <div className="bg-[rgba(193,93,255,0.8)] shadow-md py-2 px-4
+    flex justify-between">
+
+      {/* LOGO */}
+      <div>
+        <h1 className="text-4xl text-green-400 font-bold">Logo</h1>
+      </div>
+
+
+      {/* LINKS */}
+      <div className="flex justify-between items-center gap-x-4">
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/store'}>Store</NavLink>
+        <NavLink to={'/about'}>About</NavLink>
+      </div>
+
+
+      {/* CART/HAMBURGER */}
+      <div className="flex items-center">
+        <div className="relative cursor-pointer">
+          <div className="before:w-4 before:h-4 before:bg-red-600 before:rounded-full 
+        before:absolute before:bottom-0 before:right-0 before:translate-x-1/2 before:translate-y-1/2">
+            <BiCart size={26} />
+            <div className="absolute flex justify-center items-center right-0 bottom-0 translate-x-1/2 translate-y-1/2
+            text-sm font-bold">1</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
